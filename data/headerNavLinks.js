ab83dea97b7bc7f4d1b63da855131160ea368cc1
NolanKnight <nolan.r.knight@gmail.com>
2023-02-13 14:24:20 -0800

    added guides to nav

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
+  { href: '/guides', title: 'Guides' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
